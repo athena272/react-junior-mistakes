7b6243d598ffa371dc291d1ed4cacc5145112dc4
athena272 <guilhermera272@gmail.com>
2024-07-05 22:27:07 -0300

    Create index.tsx

---
+++ src/pages/Mistakes/11/index.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import { useState } from "react"
+
+export default function Mistake11() {
+    const [count, setCount] = useState(0)
+
+    // const handleClick = () => {
+    //     setCount(count + 1) // setCount(0 + 1)
+    //     setCount(count + 1) // setCount(0 + 1)
+    //     setCount(count + 1) // setCount(0 + 1)
+    //     setCount(count + 1) // setCount(0 + 1)
+    // }
+
+    const handleClick = () => {
+        setCount(prevState => prevState + 1)
+        setCount(prevState => prevState + 1)
+        setCount(prevState => prevState + 1)
+        setCount(prevState => prevState + 1)
+    }
+
+    return (
+        <main
+            className={`flex min-h-screen flex-col items-center p-24`}
+        >
+            <button
+                onClick={handleClick}
+                className="group rounded-lg border px-5 py-4 transition-colorsborder-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 text-[2rem]"
+            >
+                Click on me
+            </button>
+            <h1 className="mt-[70px] text-[2.5rem]">Count is: {count}</h1>
+        </main>
+    )
+}
