159c7cd0964111747b2967597a6923ade5293170
athena272 <guilhermera272@gmail.com>
2024-07-06 22:04:22 -0300

    Create index.tsx

---
+++ src/pages/Mistakes/9/index.tsx
@@ -0,0 +1,84 @@
+import { useState } from "react"
+
+export default function Mistake9() {
+    const [form, setForm] = useState({
+        first_name: '',
+        last_name: '',
+        email: '',
+        password: '',
+        confirm_password: ''
+    })
+
+    const handleChange = (ev: React.ChangeEvent<HTMLInputElement>) => {
+        console.log("🚀 ~ handleChange ~ ev.target.name:", ev.target.name)
+
+        setForm({
+            ...form,
+            [ev.target.name]: ev.target.value
+        })
+    }
+    console.log("🚀 ~ Mistake9 ~ form:", form)
+
+    return (
+        <form>
+            <div className="grid gap-6 mb-6 md:grid-cols-2">
+                <div>
+                    <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First name</label>
+                    <input
+                        name="first_name"
+                        type="text"
+                        id="first_name"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required
+                        onChange={handleChange}
+                        value={form['first_name']}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="last_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last name</label>
+                    <input
+                        name="last_name"
+                        type="text"
+                        id="last_name"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Doe" required
+                        onChange={handleChange}
+                        value={form['last_name']}
+                    />
+                </div>
+            </div>
+            <div className="mb-6">
+                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email address</label>
+                <input
+                    name="email"
+                    type="email"
+                    id="email"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="john.doe@company.com" required
+                    onChange={handleChange}
+                    value={form['email']}
+                />
+            </div>
+            <div className="mb-6">
+                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                <input
+                    name="password"
+                    type="password"
+                    id="password"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="•••••••••" required
+                    onChange={handleChange}
+                    value={form['password']}
+                />
+            </div>
+            <div className="mb-6">
+                <label htmlFor="confirm_password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
+                <input
+                    name="confirm_password"
+                    type="password"
+                    id="confirm_password"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="•••••••••" required
+                    onChange={handleChange}
+                    value={form['confirm_password']}
+                />
+            </div>
+            <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+        </form>
+    )
+}
