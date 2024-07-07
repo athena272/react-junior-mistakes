818a5bc7d6d5faa6eb9cf75c8c0da13e2f6cef5a
athena272 <guilhermera272@gmail.com>
2024-07-06 22:04:26 -0300

    Create index.tsx

---
+++ src/pages/Mistakes/10/index.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { useState } from "react"
+
+export default function Mistake10() {
+    const [user, setUser] = useState({
+        name: "",
+        city: "",
+        age: 50
+    })
+
+    const handleChange = (ev: React.ChangeEvent<HTMLInputElement>) => {
+        setUser({
+            ...user,
+            name: ev.target.value
+        })
+    }
+
+    console.log(user)
+
+    return (
+        <form>
+
+            <input
+                type="text"
+                placeholder="Your name"
+                className="py-[10px] px-[16px] text-black"
+                onChange={handleChange}
+            />
+        </form>
+    )
+}
