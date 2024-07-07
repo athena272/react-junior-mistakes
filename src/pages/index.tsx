d4d5749109aa3c6d1d8837ba8a53261048c86f7c
athena272 <guilhermera272@gmail.com>
2024-07-06 22:04:29 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import Mistake12 from "./Mistakes/12";
 import Mistake11 from "./Mistakes/11";
+import Mistake10 from "./Mistakes/10";
+import Mistake9 from "./Mistakes/9";
 
 export default function Home() {
   return (
     <>
-      <Mistake11 />
+      <Mistake9 />
     </>
   );
 }
