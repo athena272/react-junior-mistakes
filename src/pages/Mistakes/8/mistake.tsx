625e8c348920a4bddc09f819ce29c422f2f72af6
athena272 <guilhermera272@gmail.com>
2024-07-19 14:11:30 -0300

    Create mistake.tsx

---
+++ src/pages/Mistakes/8/mistake.tsx
@@ -0,0 +1,32 @@
+"use client"
+import { useEffect, useState } from "react"
+const PRICE_PER_ITEM = 5 
+
+export default function Mistake12() {
+    /***********************************************
+      Information can be derived from state/props
+    ************************************************/
+    const [quantity, setQuantity] = useState(0)
+    const [totalQuantity, setTotalQuantity] = useState(0)
+
+    const handleClick = () => {
+        setQuantity(quantity + 1)
+    }
+
+    useEffect(() => {
+        setTotalQuantity(quantity * PRICE_PER_ITEM)
+    }, [quantity])
+
+    return (
+        <>
+            <button
+                onClick={handleClick}
+                className="group rounded-lg border px-5 py-4 transition-colorsborder-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 text-[2rem]"
+            >
+                Add 1 item
+            </button>
+            <h1 className="mt-[70px] text-[2.5rem]">Item is: {quantity}</h1>
+            <h1 className="mt-[70px] text-[2.5rem]">Total is: {totalQuantity}</h1>
+        </>
+    )
+}
