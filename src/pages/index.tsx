cfc2617548039ff664bb1159f0279f3f8d644d06
athena272 <guilhermera272@gmail.com>
2024-07-19 14:22:50 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,14 @@ import Mistake11 from "./Mistakes/11";
 import Mistake10 from "./Mistakes/10";
 import Mistake9 from "./Mistakes/9";
 import Mistake8 from "./Mistakes/8";
+import Mistake7 from "./Mistakes/7";
 
 export default function Home() {
   return (
       <main
         className={`flex min-h-screen flex-col items-center p-24`}
       >
-        <Mistake8 />
+        <Mistake7 />
       </main>
   );
 }
